8bbd896af677ce41f8f43ccd9fc6554ffd28820e
Allen Fung <allentfung@gmail.com>
2015-09-09 14:55:47 -0700

    Set region

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,7 @@ docker push afung/hello:$SHA1
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+aws configure set region us-west-1
 aws elasticbeanstalk create-application-version --application-name hello \
     --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
